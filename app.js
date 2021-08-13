daeba3a48e4bbba7925adb8c21feaec9e9daa908
tkoleo84119 <tkoleo84119@gmail.com>
2021-08-13 22:28:51 +0800

    feat: add restaurant edit function

--- app.js
+++ app.js
@@ -75,6 +75,43 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// edit restaurant routes setting
+app.get('/restaurants/:restaurantId/edit', (req, res) => {
+  const id = req.params.restaurantId
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:restaurantId/edit', (req, res) => {
+  const id = req.params.restaurantId
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // search results routes setting
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
